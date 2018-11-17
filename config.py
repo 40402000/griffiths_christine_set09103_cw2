3f34d5bd41306a1c88ed2adbf6fe7ebd471198f7
40402000 <christine.reid83@yahoo.com>
2018-11-17 23:12:03 +0000

    login/register system complete

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-	 SECRET_KEY = os.environ.get('\xa5\x11\x06\x03q\xb4\xe5\x08o\xdb\xba9\x8fxT\xa9\xf6\xe6\x1bi\tW\x03\xd6') or 'you-will-never-guess'
+	SECRET_KEY = os.environ.get('\xa5\x11\x06\x03q\xb4\xe5\x08o\xdb\xba9\x8fxT\xa9\xf6\xe6\x1bi\tW\x03\xd6') or 'you-will-never-guess'
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
